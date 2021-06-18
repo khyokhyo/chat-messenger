1496bc6f530c5a1872f35d2a60ec96cdeaa4661f
khyokhyo <khyo.64@gmail.com>
2021-06-18 10:55:54 -0700

    Move tokens from localStorage to httpOnly cookies as per #1

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -26,6 +26,7 @@ router.post("/register", async (req, res, next) => {
       process.env.SESSION_SECRET,
       { expiresIn: 86400 }
     );
+    res.cookie("token", token, { httpOnly: true });
     res.json({
       ...user.dataValues,
       token,
@@ -64,6 +65,7 @@ router.post("/login", async (req, res, next) => {
         process.env.SESSION_SECRET,
         { expiresIn: 86400 }
       );
+      res.cookie("token", token, { httpOnly: true });
       res.json({
         ...user.dataValues,
         token,
@@ -75,6 +77,7 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.delete("/logout", (req, res, next) => {
+  res.clearCookie("token");
   res.sendStatus(204);
 });
 
